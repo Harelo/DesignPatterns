0e592e6a6b77fc098e3694cb8b080e500341bd02
Hareloo <harel123@windowslive.com>
2019-09-21 02:55:41 +0300

    Added changes to Singleton explanation

--- Knowledge/Singleton.cs
+++ Knowledge/Singleton.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Knowledge
 {
-    //This isn't thread-safe however, below is an example of a thread save Singleton
+    //A class that should only have one instance.
+    //An example for usage for this is to use a class like this as a scrabble game manager
+    //This class needs to be sealed because if for example we created a nested derived class
+    //we will be able to instantiate another instance of Singleton, and by that, this class will no longer BE a singleton class
+    //This class isn't thread-safe, an example of a thread-safe singleton can be found within the file ThreadSafeSingleton.cs
     public sealed class Singleton
     {
         private static Singleton instance = null;
--- Knowledge/TesterProgram.cs
+++ Knowledge/TesterProgram.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Knowledge
+﻿namespace Knowledge
 {
     class TesterProgram
     {
