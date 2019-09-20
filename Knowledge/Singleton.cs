c85ffb93a263d9b4216fa3bda884973120686dec
Hareloo <harel123@windowslive.com>
2019-09-21 02:46:52 +0300

    Added Thread-Safe Singleton class

--- Knowledge/Singleton.cs
+++ Knowledge/Singleton.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace Knowledge
 {
-   public sealed class Singleton
+    //This isn't thread-safe however, below is an example of a thread save Singleton
+    public sealed class Singleton
     {
         private static Singleton instance = null;
         public static Singleton GetInstance
@@ -14,6 +15,34 @@ namespace Knowledge
                 if (instance == null)
                     instance = new Singleton();
                 return instance;
+            }
+        }
+
+        public void PrintDetails(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    public sealed class ThreadSafeSingleton
+    {
+        private static Singleton instance = null;
+        private static readonly object TheLock = new object();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //Locks these lines of code on a specific object (TheLock)
+                //Makes it so only one thread can access this critical piece of code at a time
+                //Only once released can another thread access it. 
+                //This way we ensure the creation of only a single instance of the singleton class in a multi-threaded environment.
+                lock (TheLock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                    return instance;
+                }
             }
         }
 
