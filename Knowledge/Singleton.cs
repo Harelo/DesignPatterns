b68369a39fba15fd2495bf30317c5be56f5be8e2
Hareloo <harel123@windowslive.com>
2019-09-21 02:56:56 +0300

    Added explanations to the Singleton class

--- Knowledge/Singleton.cs
+++ Knowledge/Singleton.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace Knowledge
 {
+    //Explanation about this can be found here: https://www.youtube.com/watch?v=YGGg9ecy0K4
     //A class that should only have one instance.
     //An example for usage for this is to use a class like this as a scrabble game manager
-    //This class needs to be sealed because if for example we created a nested derived class
+    //This class needs to be sealed because if for example we created a nested derived class within Singleton class
     //we will be able to instantiate another instance of Singleton, and by that, this class will no longer BE a singleton class
     //This class isn't thread-safe, an example of a thread-safe singleton can be found within the file ThreadSafeSingleton.cs
     public sealed class Singleton
